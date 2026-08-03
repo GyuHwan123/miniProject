9256823e26643808f552ba9c93cd21c742f20a9b
seyeon <ksyeon068@gmail.com>
2026-08-03 10:40:43 +0900

    App.jsx기본셋팅

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
+import {BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState, useEffect } from 'react'
 
+import Header from './components/common/Header'
+import Footer from './components/common/Footer'
+import Loading from './components/common/Loading'
+import Main from './pages/Main'
+import Join from './pages/Join'
+import Login from './pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+        
+        <Header />
+        <Routes>
+            <Route path="/" element={<Main />}/>
+            <Route path="/login" element={<Login />} />
+            <Route path="/join" element={<Join />} />
+        </Routes>
+        <Footer />
       
     </>
   )
