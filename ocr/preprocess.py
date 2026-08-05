2a8598a1185c7e9b4effe829ddea3eb9540bcd58
seyeon <ksyeon068@gmail.com>
2026-08-05 17:38:58 +0900

    전처리시도중 (검증안됨)

--- ocr/preprocess.py
+++ ocr/preprocess.py
@@ -11,25 +11,49 @@ def preprocess_image(file_path):
     img = cv2.resize(
         img,
         None,
-        fx=2,
-        fy=2,
+        fx=3,
+        fy=3,
         interpolation=cv2.INTER_CUBIC
     )
 
     # 흑백 변환
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    clahe = cv2.createCLAHE(
-        clipLimit=3.0,
-        tileGridSize=(8, 8)
+    gray = cv2.cvtColor(
+        img,
+        cv2.COLOR_BGR2GRAY
     )
 
-    contrast = clahe.apply(gray)
+
+    # 밝은 이미지 어둡게 (감마)
+    gamma = 2.5
+
+    table = np.array([
+        ((i / 255.0) ** gamma) * 255
+        for i in np.arange(256)
+    ]).astype("uint8")
+
+    gamma_img = cv2.LUT(
+        gray,
+        table
+    )
+
+
+    # 대비 강화
+    clahe = cv2.createCLAHE(
+        clipLimit=3.0,
+        tileGridSize=(8,8)
+    )
+
+    contrast = clahe.apply(
+        gamma_img
+    )
+
 
     # 노이즈 제거
     denoise = cv2.fastNlMeansDenoising(
-        gray
+        contrast
     )
+
+
 
     # 이진화
     binary = cv2.threshold(
@@ -39,4 +63,36 @@ def preprocess_image(file_path):
         cv2.THRESH_BINARY + cv2.THRESH_OTSU
     )[1]
 
-    return binary
+    kernel = np.array([
+        [-1,-1,-1],
+        [-1, 9,-1],
+        [-1,-1,-1]
+    ])
+
+    sharp = cv2.filter2D(
+        denoise,
+        -1,
+        kernel
+    )
+
+    # 글자 끊김 보완
+    kernel = cv2.getStructuringElement(
+        cv2.MORPH_RECT,
+        (2,2)
+    )
+
+    closed = cv2.morphologyEx(
+        binary,
+        cv2.MORPH_CLOSE,
+        kernel
+    )
+
+    # 글자 획 굵게 (Dilation)
+    dilated = cv2.dilate(
+        closed,
+        kernel,
+        iterations=2
+    )
+
+
+    return dilated
---
+++ ocr/test_ocr.py
@@ -0,0 +1,59 @@
+from paddleocr import PaddleOCR
+from preprocess import preprocess_image
+import cv2
+
+
+ocr = PaddleOCR(
+    lang="korean",
+    use_angle_cls=True
+)
+
+
+input_path = "test_images/3-1.jpg"
+
+
+# =====================
+# 1. 원본 이미지 OCR
+# =====================
+
+print("\n===== 원본 OCR =====")
+
+original_result = ocr.ocr(input_path)
+
+
+for line in original_result:
+    for item in line:
+        text = item[1][0]
+        score = item[1][1]
+
+        print(text, ":", round(score, 2))
+
+
+# =====================
+# 2. 전처리 이미지 OCR
+# =====================
+
+print("\n===== 전처리 OCR =====")
+
+
+processed = preprocess_image(input_path)
+
+
+# 임시 저장
+cv2.imwrite(
+    "processed.png",
+    processed
+)
+
+
+processed_result = ocr.ocr(
+    "processed.png"
+)
+
+
+for line in processed_result:
+    for item in line:
+        text = item[1][0]
+        score = item[1][1]
+
+        print(text, ":", round(score, 2))
